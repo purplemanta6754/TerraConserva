63b58f793e86515e02c0deed1d1c9cdf01aea539
purplemanta6754 <purplemanta6754@users.noreply.github.com>
2023-06-12 18:37:01 -0400

    Código no funcional eliminado

--- js/scrollspy.js
+++ js/scrollspy.js
@@ -8,35 +8,3 @@ function myFunction() {
         x.className = "topnav";
     }
 }
-
-// Obtener todos los elementos de navegación en la lista de secciones
-const navItems = document.querySelectorAll('nav a');
-
-// Obtener todas las secciones en la página
-const sections = document.querySelectorAll('main section');
-
-// Función para agregar o quitar una clase activa en el elemento de navegación correspondiente
-function activateNavItem(index) {
-    navItems.forEach(item => item.classList.remove('active'));
-    navItems[index].classList.add('active');
-}
-
-// Función para comprobar qué sección está visible en la ventana
-function checkSectionInView() {
-    const scrollPosition = window.scrollY;
-
-    sections.forEach((section, index) => {
-        const sectionTop = section.offsetTop;
-        const sectionHeight = section.offsetHeight;
-
-        if (scrollPosition >= sectionTop && scrollPosition < sectionTop + sectionHeight) {
-            activateNavItem(index);
-        }
-    });
-}
-
-// Llamada inicial para establecer la clase activa en el elemento de navegación actual
-checkSectionInView();
-
-// Agregar un evento de desplazamiento para comprobar qué sección está visible en la ventana mientras se desplaza
-window.addEventListener('scroll', checkSectionInView);
